4d980a9bdd98f6b2f998514bcc13a5848d4f7bf2
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2022-11-30 15:27:09 +0200

    Update LUDO.py

--- LUDO.py
+++ LUDO.py
@@ -17,18 +17,19 @@ data_II = pd.DataFrame([((A/Total)*100),((M/Total)*100)],columns=['Score %'])
 data_II=data_II.rename(columns = {'0':'index'})
 
 data_III = pd.concat([data_I,data_II], axis = 1)
+ 
+if Total >0: 
+  st.write(data_III)
 
-st.write(data_III)
+  fig= px.bar(data_I, y = 'Score' ) 
+  st.plotly_chart(fig, use_container_width=True)
 
-fig= px.bar(data_I, y = 'Score' ) 
-st.plotly_chart(fig, use_container_width=True)
+  # streamlit run "C:\\Users\\hp\\Desktop\\Data Science\\Test\\LUDO.py"
 
-# streamlit run "C:\\Users\\hp\\Desktop\\Data Science\\Test\\LUDO.py"
+  st.write(f'The Total Matches = {A+M} ')
 
-st.write(f'The Total Matches = {A+M} ')
-
-st.write(f'The Total Matches of Abdallah  = {A}  Matches with Percentage {(A/Total)*100} %')
-st.write(f'The Total Matches of Abdallah  = {M}  Matches with Percentage {(M/Total)*100} %')
+  st.write(f'The Total Matches of Abdallah  = {A}  Matches with Percentage {(A/Total)*100} %')
+  st.write(f'The Total Matches of Abdallah  = {M}  Matches with Percentage {(M/Total)*100} %')
 
 
 
